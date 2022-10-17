425eeee87009be99d1cb92142bbf9c1e54e77944
myouse19 <113076617+myouse19@users.noreply.github.com>
2022-10-16 20:27:50 -0700

    added for loop in the generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,7 @@ var AllCharacters = LowerCaseCharacters + UpperCaseCharacters + Numbers + Specia
 var UserChoice = [];
 // Write password to the #password input
 function writePassword() {
+  if (Prompts) 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -24,14 +25,31 @@ generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 
 function generatePassword() {
-  CharacterLength = parseint(prompt("How many characters do you want your password to be?"))
+  var password = "";
+  for (i = 0; i < CharacterLength; i++) {
+    var randomchoice = Math.floor(Math.random * UserChoice.length)
+    password = password + UserChoice[randomchoice];
+  };
+  return password;
+};
+
+
+function Prompts() {
+  CharacterLength = parseInt(prompt("How many characters do you want your password to be?"))
   if (isNaN(CharacterLength || CharacterLength < 8 || CharacterLength > 128)) {
     return false
   }
 if (confirm("Do you want to include lowercase characters?")) {
   UserChoice = UserChoice.concat(LowerCaseCharacters)
 }
-if (confirm("Do you want to include uppercase characters?")
-confirm("Do you want to include numeric characters?");
-confirm("Do you want to include special characters?");
+if (confirm("Do you want to include uppercase characters?")) {
+  UserChoice = UserChoice.concat(UpperCaseCharacters)
+}
+if (confirm("Do you want to include numeric characters?")) {
+  UserChoice = UserChoice.concat(Numbers)
+}
+if (confirm("Do you want to include special characters?")) {
+  UserChoice = UserChoice.concat(SpecialCharacters)
+}
+return true
 }
