bb28678f2de1d7ecf55026865f54e525dfcd095f
myouse19 <113076617+myouse19@users.noreply.github.com>
2022-10-16 22:28:04 -0700

    modified the prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,40 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+///////////////////////// DO NOT CHANGE ////////////////////////////////////
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var CharacterLength = 8;
-var LowerCaseCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-var UpperCaseCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z' ];
-var Numbers = [ '1','2','3','4','5','6','7','8','9','0' ];
-var SpecialCharacters = [ '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '"' ];
-var AllCharacters = LowerCaseCharacters + UpperCaseCharacters + Numbers + SpecialCharacters;
-var UserChoice = [];
+
 // Write password to the #password input
 function writePassword() {
-  if (Prompts) 
-  var password = generatePassword();
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
+var CharacterLength = 8;
+var LowerCaseCharacters = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+var UpperCaseCharacters =  ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+"M", "N", "O", "P", "Q", "R",  "S", "T", "U", "V", "W", "X",
+"Y", "Z")
+var Numbers = ("1","2","3","4","5","6","7","8","9","0" )
+var SpecialCharacters =  ("!", "@", "#", "$", "%", "^", "&", "*", "(", ")" )
+var UserChoice = [];
+var AllCharacters = LowerCaseCharacters + UpperCaseCharacters + Numbers + SpecialCharacters
 
-function generatePassword() {
-  var password = "";
-  for (i = 0; i < CharacterLength; i++) {
-    var randomchoice = Math.floor(Math.random * UserChoice.length)
-    password = password + UserChoice[randomchoice];
-  };
-  return password;
-};
+function generatePassword() { 
+  var Newpassword = "";
+  for (var i = 0; i < CharacterLength; i++) {
+    var RandomChoice = Math.floor(Math.random() * AllCharacters.length);
+    Newpassword = Newpassword + AllCharacters[RandomChoice];
+  }
+  return Newpassword
+}
 
 
 function Prompts() {
+  UserChoice = [];
   CharacterLength = parseInt(prompt("How many characters do you want your password to be?"))
   if (isNaN(CharacterLength || CharacterLength < 8 || CharacterLength > 128)) {
     return false
@@ -53,3 +53,5 @@ if (confirm("Do you want to include special characters?")) {
 }
 return true
 }
+
+Prompts()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Password Generator Starter Code
+added variables for each type of characters
+combined all characters in one variable
+added all the readme prompts
+url: file:///Users/mohamadyousef/Desktop/JS-StarterCode/JS-StarterCode/Develop/index.html
